d41939d8ad0143617d2590d7dcb2b8d5acc6976d
ajay162-hash <180277581+ajay162-hash@users.noreply.github.com.>
2025-07-05 15:34:04 +0530

    Temporarily remove toast-ui editor for deployment

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,20 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isPublicRoute = createRouteMatcher([
+  '/',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+]);
 
 export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) auth().protect()
-  })
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', "/"])
+  if (!isPublicRoute(req)) {
+    auth().protect();
+  }
+});
+
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+    '/((?!_next|.*\\..*).*)',
     '/(api|trpc)(.*)',
   ],
-}
+};
